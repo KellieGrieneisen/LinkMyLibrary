a918a518a0f9f24d1134662be0c9dbc245b023fe
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-06-01 18:21:48 +0000

    first commit

---
+++ server.py
@@ -0,0 +1,19 @@
+""" Server for Link my Library"""
+from flask import (Flask,render_template, request, flash, session,
+                   redirect)
+from model import 
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    """Display the homepage."""
+
+    return render_template('homepage.html')
+
+
+
+if __name__ == '__main__':
+    connect_to_db(app)
+    app.run(host='0.0.0.0', debug=True)
