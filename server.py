0c60a13580970be973e1507c37505a31f8230f4c
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-06-01 20:05:02 +0000

    created homepage/login template

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 """ Server for Link my Library"""
-from flask import (Flask,render_template, request, flash, session,
+from flask import (Flask, render_template, request, flash, session,
                    redirect)
-from model import 
+
+# from database import session as db_session
+
 
 app = Flask(__name__)
 
@@ -12,9 +14,13 @@ def homepage():
 
     return render_template('homepage.html')
 
+@app.route('/login')
+def handle_login():
+    """Check input against user_id's and redirect to user library"""
+
 
 
 
 if __name__ == '__main__':
-    connect_to_db(app)
+    # connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
