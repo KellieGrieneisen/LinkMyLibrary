8e8d4f4611957c0194440c2096d479ba746e4a61
Kellie Grieneisen <kellie_grieneisen@aol.com>
2021-06-02 19:07:47 +0000

    successful link from homepage to user library once logged in! User class working in model.py, bugs in Book and Author classes.

--- server.py
+++ server.py
@@ -3,28 +3,29 @@ from flask import (Flask, render_template, request, flash, session,
                    redirect)
 
 # from database import session as db_session
-
+from model import connect_to_db
 
 app = Flask(__name__)
+app.secret_key = "dev"
+# app.jinja_env.undefined = StrictUndefined
 
-
-@app.route('/')
+@app.route('/', methods=["GET"])
 def homepage():
-    """Display the homepage."""
+    """Display the homepage and login."""
 
     return render_template('homepage.html')
 
-@app.route('/login', methods=["POST"])
+@app.route('/', methods=["POST"])
 def handle_login():
     """Check input against user_id's and redirect to user library"""
     email = request.form.get('email')
     password = request.form.get('password')
 
-    # user = 
+    
 
     return redirect('/user-library')
 
-@app.route('/add-book')
+@app.route('/add-book', methods=["GET"])
 def add_new_book():
     """Add new book to user library."""
 
@@ -34,6 +35,8 @@ def add_new_book():
 def show_library():
     """View users library."""
 
+    return render_template('user_library.html')
+
 if __name__ == '__main__':
-    # connect_to_db(app)
+    connect_to_db(app)
     app.run(host='0.0.0.0', debug=True)
